3c40d311a19bfb80f61af5427ea63361a41495cb
OKOPOK <self@BOPOHOB.ru>
2020-07-22 04:54:54 +0300

    small menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -2,52 +2,54 @@ import React from 'react';
 import { Menu } from 'antd';
 import { inject, observer } from 'mobx-react';
 import { useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
 import data from './data';
 import Item from './item';
 import Submenu from './submenu';
 import style from './style.module.scss';
 
-const MenuComponent = inject('menu')(observer((props) => {
-  const { pathname: currentPathname } = useLocation();
-  const findCurrentElement = () => {
-    for (const { act, text } of data) {
-      if (Array.isArray(act)) {
-        for (const { act: itemAct, text: itemText } of act) {
-          if (typeof itemAct === 'object' && itemAct.path === currentPathname) {
-            return {
-              defaultSelectedKeys: [itemText],
-              defaultOpenKeys: [text],
-            };
-          }
+const findCurrentElement = (currentPathname) => {
+  for (const { act, text } of data) {
+    if (Array.isArray(act)) {
+      for (const { act: itemAct, text: itemText } of act) {
+        if (typeof itemAct === 'object' && itemAct.path === currentPathname) {
+          return {
+            defaultSelectedKeys: [itemText],
+            defaultOpenKeys: [text],
+          };
         }
-      } else if (typeof act === 'object' && act.path === currentPathname) {
-        return {
-          defaultSelectedKeys: [text],
-          defaultOpenKeys: [],
-        };
       }
+    } else if (typeof act === 'object' && act.path === currentPathname) {
+      return {
+        defaultSelectedKeys: [text],
+        defaultOpenKeys: [],
+      };
     }
-    return {
-      defaultSelectedKeys: [],
-      defaultOpenKeys: [],
-    };
+  }
+  return {
+    defaultSelectedKeys: [],
+    defaultOpenKeys: [],
   };
-  const { defaultSelectedKeys, defaultOpenKeys } = findCurrentElement();
+};
+
+const MenuComponent = inject('menu')(observer(({ menu }) => {
+  const { pathname } = useLocation();
+  const { defaultSelectedKeys, defaultOpenKeys } = findCurrentElement(pathname);
   return (
     <Menu
       defaultSelectedKeys={defaultSelectedKeys}
       defaultOpenKeys={defaultOpenKeys}
-      mode="inline"
+      mode={menu.mode}
       className={style.menu}
     >
       {
         data.map(({ icon, text, act }) => {
           if (Array.isArray(act)) {
             console.assert(typeof icon !== 'undefined');
-            return <Submenu key={text} icon={icon} items={act}>{text}</Submenu>;
+            return <Submenu key={text} icon={icon} items={act}>{menu.isOpen && text}</Submenu>;
           }
-          return <Item icon={icon} key={text} act={act}>{text}</Item>;
+          return <Item icon={icon} key={text} act={act}>{menu.isOpen && text}</Item>;
         })
       }
     </Menu>
--- src/models/menu.js
+++ src/models/menu.js
@@ -7,14 +7,14 @@ const MENU_OPEN_STORAGE_KEY = 'is_menu_open';
 
 class Menu {
   @computed get isOpen() {
-    return localStorage.getItem(MENU_OPEN_STORAGE_KEY);
+    return !localStorage.getItem(MENU_OPEN_STORAGE_KEY);
   }
 
   set isOpen(val) {
-    localStorage.setItem(MENU_OPEN_STORAGE_KEY, val);
+    localStorage.setItem(MENU_OPEN_STORAGE_KEY, !val);
   }
 
-  @computed get mode() { return this.isOpen ? 'vertical' : 'inline'; }
+  @computed get mode() { return this.isOpen ? 'inline' : 'vertical'; }
 }
 
 export default Menu;
