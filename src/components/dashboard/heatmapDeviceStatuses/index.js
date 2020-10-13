328df0af8ce0083be36eb0f79a55477fb4fb70ef
Анатолий Хахалин <wequbelity@gmail.com>
2020-10-13 22:33:51 +0300

    feat(heatmapDeviceStatuses): first version of tooltip added (full information will be display in modal)

--- src/components/dashboard/heatmapDeviceStatuses/index.js
+++ src/components/dashboard/heatmapDeviceStatuses/index.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { inject, observer } from 'mobx-react';
+import { Modal } from 'antd';
 
-import Heatmap from 'elements/chart/heatmap';
+import Heatmap from 'elements/chart/heatmap/index';
 import Loader from 'elements/loader';
 
 import classnames from './index.module.scss';
 
-function Legend() {
-  return <div>Legend</div>;
-}
+const legend = [
+  {
+    from: 1,
+    to: 1,
+    name: 'Активно',
+    color: '#00A100',
+  },
+  {
+    from: 2,
+    to: 2,
+    name: 'Предупреждение',
+    color: '#FFB200',
+  },
+  {
+    from: 3,
+    to: 3,
+    name: 'Ошибка',
+    color: '#FF0000',
+  },
+];
+
+const Tooltip = ({ name, salePoint, isOn }) => (
+  <div>
+    <h3>{name}</h3>
+  </div>
+);
+
+const createTooltip = ({ onChange, chartData }) => ({
+  series, seriesIndex, dataPointIndex, w,
+}) => ReactDOMServer.renderToString(<Tooltip name="Оборудование" salePoint="Точка продаж" isOn />);
 
 const Chart = inject('storage')(observer(({
   storage,
 }) => {
   const { isLoaded, chartData } = storage;
-  const legend = [{
-    from: 1,
-    to: 1,
-    name: 'Активно',
-    color: '#00A100',
-  }];
+  const [selected, setSelected] = useState(null);
+  const tooltip = useCallback(createTooltip({ chartData }), [chartData]);
 
   if (!isLoaded) {
     return <Loader size="large" />;
@@ -27,12 +52,21 @@ const Chart = inject('storage')(observer(({
 
   return (
     <div className={classnames.root}>
-      <Heatmap legend={legend} data={chartData} />
-      <Legend />
-      <div>
-        Оборудования найдено:
-        {chartData.length}
-      </div>
+      <Heatmap
+        onSelect={(event, chartContext, config) => {
+          const { w, seriesIndex, dataPointIndex } = config;
+          setSelected(w.config.series[seriesIndex].data[dataPointIndex].x);
+        }}
+        legend={legend}
+        data={chartData}
+        tooltipTemplate={tooltip}
+      />
+
+      <Modal visible={Boolean(selected)} onCancel={() => setSelected(null)} onOk={() => setSelected(null)}>
+        <div>
+          {selected}
+        </div>
+      </Modal>
     </div>
   );
 }));
--- src/models/dashboard/widgets/heatmapDeviceStatuses.js
+++ src/models/dashboard/widgets/heatmapDeviceStatuses.js
@@ -14,10 +14,15 @@ class HeatmapDeviceStatuses {
     return this.session.points.isLoaded && this.session.devices.isLoaded;
   }
 
-  @computed({ keepAlive: true }) get devices() {
-    return this.session.devices.rawData.toJSON()
-      .map((d) => ({ ...d, state: Math.floor(Math.random() * 3 + 1) }))
-      .map((d) => ({ name: d.id, value: d.state }));
+  @computed({ keepAlive: true }) get chartData() {
+    if (!Array.isArray(this.generic.devices)) {
+      return [];
+    }
+
+    return this.generic.devices
+      .map((d) => ({ ...d, state: Math.floor(Math.random() * 3 + 1) })) // TODO remove when state come from server
+      .map((d) => ({ name: d.id, value: d.state }))
+      .sort((a, b) => a.value - b.value);
   }
 }
 
