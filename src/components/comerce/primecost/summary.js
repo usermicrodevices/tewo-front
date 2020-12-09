a1f0ad7d64bedaf9e509e3489aa985b7e42c4c85
OKOPOK <self@BOPOHOB.ru>
2020-12-09 22:19:02 +0300

    primecost summary

--- src/components/comerce/primecost/summary.js
+++ src/components/comerce/primecost/summary.js
@@ -2,11 +2,60 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 
 import SummaryCard from 'elements/card/summary';
+import Typography from 'elements/typography';
+import Format from 'elements/format';
+
+import classes from './summary.module.scss';
+
+const Row = ({ earn, cost, margin }) => (
+  <div className={classes.row}>
+    <div>
+      <Typography.Value size="xl">
+        <Format>{ earn }</Format>
+        ₽
+      </Typography.Value>
+      <Typography.Caption className={classes.caption}>выручка</Typography.Caption>
+    </div>
+    <div>
+      <Typography.Value size="xl">
+        <Format>{ cost }</Format>
+        ₽
+      </Typography.Value>
+      <Typography.Caption className={classes.caption}>себестоимость</Typography.Caption>
+    </div>
+    <div>
+      <Typography.Value size="xl">
+        <Format>{ margin }</Format>
+        ₽
+      </Typography.Value>
+      <Typography.Caption className={classes.caption}>прибыль</Typography.Caption>
+    </div>
+  </div>
+);
 
 const Summary = ({ table }) => {
+  const { summary, top } = table;
   return (
     <SummaryCard>
-      Сводная информация
+      <div>
+        <div className={classes.title}><Typography.Title level={4}>Общие данные по напиткам</Typography.Title></div>
+        <Row earn={summary.earn} cost={summary.cost} margin={summary.margin} />
+      </div>
+      <div className={classes.top}>
+        <div className={classes.title}><Typography.Title level={4}>Топ 5 напитков по прибыли</Typography.Title></div>
+        <div>
+          {
+            top.slice(0, 5).map(({
+              id, name, earn, cost, margin,
+            }, index) => (
+              <div key={id}>
+                <div className={classes.name}><Typography.Title level={2}>{`${index + 1} ${name}`}</Typography.Title></div>
+                <Row earn={earn} cost={cost} margin={margin} />
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </SummaryCard>
   );
 };
--- src/models/comerce/primecost.js
+++ src/models/comerce/primecost.js
@@ -1,5 +1,5 @@
 /* eslint class-methods-use-this: off */
-import { observable } from 'mobx';
+import { computed, observable } from 'mobx';
 
 import Filters from 'models/filters';
 import Table from 'models/table';
@@ -30,9 +30,43 @@ const declareColumns = () => ({
 });
 
 class PrimeCost extends Table {
-  @observable cleans;
+  @computed get summary() {
+    const result = {
+      earn: 0,
+      cost: 0,
+      margin: 0,
+    };
+    for (const { earn, cost, margin } of this.rawData) {
+      result.earn += earn;
+      result.cost += cost;
+      result.margin += margin;
+    }
+    return result;
+  }
 
-  @observable stats = {};
+  @computed get top() {
+    const result = {};
+    for (const { data, session } of this.rawData) {
+      for (const point of Object.values(data.details)) {
+        for (const drink of Object.values(point.details)) {
+          if (!(drink.id in result)) {
+            result[drink.id] = {
+              id: drink.id,
+              name: session.drinks.get(drink.id)?.name,
+              earn: 0,
+              cost: 0,
+              margin: 0,
+            };
+          }
+          const datum = result[drink.id];
+          datum.earn += drink.earn;
+          datum.cost += drink.cost;
+          datum.margin += drink.margin;
+        }
+      }
+    }
+    return Object.values(result).sort(({ margin: a }, { margin: b }) => b - a);
+  }
 
   constructor(session) {
     const filters = new Filters(DECLARE_BEVERAGES_FILTERS(session));
