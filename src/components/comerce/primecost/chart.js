006f3f06ceb302946c36543b6b301afa278d1f74
OKOPOK <self@BOPOHOB.ru>
2020-12-10 02:38:15 +0300

    primecost chart draft

--- src/components/comerce/primecost/chart.js
+++ src/components/comerce/primecost/chart.js
@@ -1,13 +1,82 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { Card } from 'antd';
+import Chart from 'react-apexcharts';
+import { withSize } from 'react-sizeme';
 
-const Chart = ({ table }) => {
+import colors from 'themes/chart';
+import NoData from 'elements/noData';
+
+import locale from 'elements/chart/locale';
+import style from 'elements/chart/style.module.scss';
+
+const PrimecostChart = inject('table')(observer(({
+  size, table,
+}) => {
+  if (!table.isLoaded) {
+    return <NoData noMargin title="Недостаточно данных для построения графика" />;
+  }
+  const { categories, series } = table.chart;
+
+  const data = {
+    colors,
+    chart: {
+      height: size.height,
+      type: 'bar',
+      stacked: true,
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      ...locale,
+    },
+
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: true,
+    },
+    stroke: {
+      width: 2,
+    },
+    xaxis: {
+      categories,
+    },
+    yaxis: {
+      title: {
+        text: undefined,
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    legend: {
+      horizontalAlign: 'left',
+      offsetX: 40,
+    },
+  };
   return (
-    <Card>
-      График
-    </Card>
+    <Chart
+      type="bar"
+      series={series}
+      width={size.width}
+      height={data.chart.height}
+      options={data}
+    />
   );
-};
+}));
 
-export default inject('table')(observer(Chart));
+const Wrap = withSize()(({
+  size: { width },
+}) => (
+  <Card>
+    <div className={style.chartwrap}><PrimecostChart size={{ width: width - 50, height: 421 }} /></div>
+  </Card>
+));
+
+export default inject('table')(observer(Wrap));
--- src/models/comerce/primecost.js
+++ src/models/comerce/primecost.js
@@ -68,12 +68,78 @@ class PrimeCost extends Table {
     return Object.values(result).sort(({ margin: a }, { margin: b }) => b - a);
   }
 
+  @computed get chart() {
+    const DRINKS_AMOUNT = 6;
+    const { session } = this;
+    const result = {};
+    for (const { data } of this.rawData) {
+      for (const point of Object.values(data.details)) {
+        for (const drink of Object.values(point.details)) {
+          if (!(drink.id in result)) {
+            const ingredients = {};
+            for (const ingredientId of Object.keys(drink.details)) {
+              ingredients[ingredientId] = 0;
+            }
+            result[drink.id] = {
+              name: session.drinks.get(drink.id)?.name,
+              margin: 0,
+              data: ingredients,
+            };
+          }
+          const datum = result[drink.id];
+          /**
+           * Получается, что мы у игридиентов складываем себестоимость и
+           * дополняем маржой
+           */
+          for (const { id, cost } of Object.values(drink.details)) {
+            datum.data[id] += cost;
+          }
+          datum.margin += drink.margin;
+        }
+      }
+    }
+    const orderedResult = Object.entries(result).sort(([_, { margin: a }], [__, { margin: b }]) => b - a).slice(0, DRINKS_AMOUNT);
+    const categories = orderedResult.slice(0, DRINKS_AMOUNT).map(([drinkId]) => session.drinks.get(parseInt(drinkId, 10))?.name);
+    const usedIngredientsSet = new Set();
+    for (const [_, { data }] of orderedResult) {
+      for (const ingredientId of Object.keys(data)) {
+        usedIngredientsSet.add(parseInt(ingredientId, 10));
+      }
+    }
+    const usedIngredientsIds = [...usedIngredientsSet.values()];
+    const series = usedIngredientsIds.map((id) => ({
+      name: session.ingredients.get(id)?.name,
+      data: new Array(orderedResult.length),
+    }));
+    series.push({
+      name: 'Прибыль',
+      data: new Array(orderedResult.length),
+    });
+    for (const [ingredientIndex, ingredientId] of usedIngredientsIds.entries()) {
+      const seria = series[ingredientIndex];
+      for (const [drinkIndex, [_, { data }]] of orderedResult.entries()) {
+        seria.data[drinkIndex] = data[ingredientId] || 0;
+      }
+    }
+    for (const [drinkIndex, [_, { margin }]] of orderedResult.entries()) {
+      series[series.length - 1].data[drinkIndex] = margin;
+    }
+    return {
+      categories,
+      series,
+    };
+  }
+
+  session;
+
   constructor(session) {
     const filters = new Filters(DECLARE_BEVERAGES_FILTERS(session));
 
     filters.isShowSearch = false;
 
     super(declareColumns(session), getPrimecost(session), filters);
+
+    this.session = session;
   }
 
   toString() {
