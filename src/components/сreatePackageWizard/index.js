effa3ced5da523bc4caa52832afd87e59a31f14f
OKOPOK <self@BOPOHOB.ru>
2021-04-18 18:54:12 +0300

    feat(packages) двухколоночная форма создания сессии

--- src/components/сreatePackageWizard/index.js
+++ src/components/сreatePackageWizard/index.js
@@ -1,29 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { Modal } from 'antd';
 
 import plural from 'utils/plural';
 import SelectableTable from 'elements/table/selectableTable';
+import Selector from 'elements/filters/select';
 
 import classNames from './css.module.scss';
 
 const Wizard = inject('packetsHolder')(observer(({ packetsHolder }) => {
-  const [stage, setStage] = useState(0);
-  const onNext = () => {
-    if (stage === 0) {
-      setStage(1);
-      return;
-    }
-    packetsHolder.submitnewSession();
+  if (packetsHolder.newSession === null) {
+    return null;
+  }
+  const onCancel = () => {
+    packetsHolder.clearNewSession();
   };
-  const onPrew = () => {
-    if (stage === 0) {
-      packetsHolder.clearNewSession();
-      return;
-    }
-    setStage(0);
+  const onOk = () => {
+    packetsHolder.submitNewSession();
   };
-  const selectedDevicesCount = packetsHolder.newSession?.devices?.size;
+  const selectedDevicesCount = packetsHolder.newSession.devices.size;
   const onSelectDevice = (devices) => {
     // eslint-disable-next-line no-param-reassign
     packetsHolder.newSession.devices = devices;
@@ -39,59 +34,53 @@ const Wizard = inject('packetsHolder')(observer(({ packetsHolder }) => {
     }) => ({
       key: id, name, salePointName, serial, companyName,
     })) : undefined;
-  const okText = [
-    selectedDevicesCount ? 'Далее' : 'Устройства не выбраны',
-    `Загрузить на ${plural(selectedDevicesCount, ['устройство', 'устройства', 'устройства'])} (${selectedDevicesCount} / ${ds?.length})`,
-  ];
-  const okType = ['default', 'primary'];
-  const cancelText = ['Отмена', 'Назад'];
+  console.log(selectedDevicesCount === 0 && packetsHolder.newSession.packet, selectedDevicesCount === 0, packetsHolder.newSession.packet);
   return (
     <Modal
-      title="Загрузка пакета"
-      visible={packetsHolder.newSession !== null}
-      onOk={onNext}
-      onCancel={onPrew}
-      okText={okText[stage]}
-      okType={okType[stage]}
-      cancelText={cancelText[stage]}
-      okButtonProps={{ disabled: selectedDevicesCount === 0 }}
+      title={`Загрузка пакета (${selectedDevicesCount} ${plural(selectedDevicesCount, ['устройство', 'устройств', 'устройства'])})`}
+      visible
+      onOk={onOk}
+      onCancel={onCancel}
       width="80vw"
+      okButtonProps={{ disabled: selectedDevicesCount === 0 || packetsHolder.newSession.packet === null }}
       bodyStyle={{
         maxHeight: '80vh',
       }}
+      className={classNames.wrap}
     >
-      {
-        stage === 0 && (
-          <SelectableTable
-            className={classNames.table}
-            onSelect={onSelectDevice}
-            columns={{
-              serial: {
-                title: 'Серийный номер',
-                grow: 2,
-              },
-              name: {
-                title: 'Название',
-                grow: 2,
-              },
-              salePointName: {
-                title: 'Объект',
-                grow: 2,
-              },
-              companyName: {
-                title: 'Компания',
-                grow: 2,
-              },
-            }}
-            dataSource={ds}
-          />
-        )
-      }
-      {
-        stage === 1 && (
-          'not implemented facepunch'
-        )
-      }
+      <SelectableTable
+        className={classNames.table}
+        onSelect={onSelectDevice}
+        columns={{
+          serial: {
+            title: 'Серийный номер',
+            grow: 2,
+          },
+          name: {
+            title: 'Название',
+            grow: 2,
+          },
+          salePointName: {
+            title: 'Объект',
+            grow: 2,
+          },
+          companyName: {
+            title: 'Компания',
+            grow: 2,
+          },
+        }}
+        dataSource={ds}
+      />
+      <div>
+        <Selector
+          title="Пакет"
+          isSingle
+          value={packetsHolder.newSession.packet}
+          // eslint-disable-next-line no-param-reassign
+          onChange={(packet) => { console.log(packet); packetsHolder.newSession.packet = packet || null; }}
+          selector={packetsHolder.packets.selector}
+        />
+      </div>
     </Modal>
   );
 }));
