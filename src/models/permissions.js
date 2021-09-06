3b670f7ceb6864ec76b0b395b030d59ea7ce7e9b
BOPOHOB <bopohob@yandex-team.ru>
2021-09-06 10:20:31 +0300

    beverages remove

--- src/models/permissions.js
+++ src/models/permissions.js
@@ -1,11 +1,19 @@
-import { observable } from 'mobx';
+import { observable, computed } from 'mobx';
 import { getPermissions as getPackagesPermissions } from 'services/packages';
 
 class Permissions {
-  @observable packages = []
+  @observable packages = [];
 
   constructor() {
     this.getPackagesPermissions();
+  }
+
+  checkPermission(permission) {
+    return Boolean(this.packages.find((perm) => perm === permission));
+  }
+
+  @computed get isAllowDelete() {
+    return this.checkPermission('delete');
   }
 
   getPackagesPermissions = () => {
